32d4af7a6c73658de39ab7491ebdaf9f1c027c38
Shichen Lu <shichenl2@gmail.com>
2019-06-15 16:55:07 -0400

    Working ish model

--- model.py
+++ model.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_probability as tfp
+from tensorflow import keras as K
 from tensorflow.python.keras import layers
-from tensorflow.python.keras import models
 import robot_doors_experiment as exp
 import time
 
@@ -11,52 +11,97 @@ tf.enable_eager_execution()
 tfe = tf.contrib.eager
 tfd = tfp.distributions
 
+def create_qRNN_model(latent_dim, input_timesteps):
+    # Use functional interface to define posterior model.
+    # Inputs: observation and previous state, Outputs: current state as variational posterior (mean and var)
+    observation = K.Input(shape=(input_timesteps, 5), name='observation')
+    prev_state = K.Input(shape=(latent_dim,), name='prev_state')
+    latent_state = K.layers.LSTM(units=10, input_shape=(input_timesteps, 5), name = 'latent_state')(observation)
+    latent_prev_states = K.layers.concatenate([latent_state, prev_state])
+    curr_state = K.layers.Dense(units=latent_dim * 2, activation='softplus', name='curr_state')(
+        latent_prev_states)
+    posterior_model = K.Model(inputs=[observation, prev_state], outputs=curr_state)
+    return posterior_model
 
-class DELIP_model(tf.keras.Model):
-    def __init__(self, latent_dim=4): # Eliminate magic numbers
+def create_qBRNN_model(latent_dim, input_timesteps):
+    # Use functional interface to define posterior model.
+    # Inputs: observation and previous state, Outputs: current state as variational posterior (mean and var)
+    observation = K.Input(shape=(input_timesteps, 5), name='observation')
+    rnn_state = K.layers.Bidirectional(
+        K.layers.LSTM(units=10, input_shape=(input_timesteps, 5)), name='rnn_state'
+    )(observation)
+    latent_state = K.layers.Dense(units=latent_dim * 2, activation='softplus', name='latent_state')(rnn_state)
+    latent_sample = K.layers.Lambda(reparameterize, output_shape=(latent_dim,), name='latent_sample')(latent_state)
+    observations = K.layers.Dense(units=100, activation='relu', name='obs1')(latent_sample)
+    tf.layers.Dense(units=100, activation='relu'),
+    tf.layers.Dense(units=100, activation='relu'),
+    tf.layers.Dense(units=100, activation='relu'),
+    tf.layers.Dense(units=3 * 2, activation='softplus'),
+
+
+    posterior_model = K.Model(inputs=observation, outputs=curr_state)
+    decoder_model = K.Model(inputs=latent_state, outputs=[observations, rewards])
+    return posterior_model
+
+
+class DELIP_model(K.Model):
+    def __init__(self, latent_dim, input_timesteps): # TODO(slu): Eliminate magic numbers
         super(DELIP_model, self).__init__()
+        self.input_timesteps = input_timesteps
         self.latent_dim = latent_dim
         self.last_latent_state = None
 
-        # Use functional interface to define posterior model.
-        # Inputs: observation and previous state, Outputs: current state as variational posterior (mean and var)
-        observation = tf.keras.Input(shape=(1,5), name='observation')
-        prev_state = tf.keras.Input(shape=(self.latent_dim,), name='prev_state')
-        # latent_state = tf.keras.layers.Bidirectional( # TODO(slu): check bidirectional functionality, bidirectional or single direction and add s_t-1?
-        #     tf.keras.layers.LSTM(units=10, input_shape=(1,5)), name='latent_state'
-        # )(observation)
-        latent_state = tf.keras.layers.LSTM(units=10, input_shape=(1, 5), name = 'latent_state')(observation)
-        latent_prev_states = tf.keras.layers.concatenate([latent_state, prev_state])
-        curr_state = tf.keras.layers.Dense(units=self.latent_dim*2, activation='softplus', name='curr_state')(latent_prev_states)
-        # TODO(slu): softplus? check danijar
-        self.posterior_model = tf.keras.Model(inputs=[observation, prev_state], outputs=curr_state)
+        timestep_data = K.Input(shape=(self.input_timesteps, 5), name='timestep_data')
+        rnn_state = K.layers.Bidirectional(
+            K.layers.LSTM(units=10, input_shape=(self.input_timesteps, 5), return_sequences=True),
+            name='rnn_state'
+            )(timestep_data)
+        latent_state = K.layers.TimeDistributed(
+            K.layers.Dense(units=self.latent_dim*2, activation='softplus'),
+            name='latent_state'
+            )(rnn_state)
 
-        self.next_state_model = tf.keras.Sequential(
-            [
-                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
-                tf.keras.layers.Dense(units=self.latent_dim*2, activation='linear')
-            ]
-        )
+        decoder_in = K.layers.Input(shape=(self.input_timesteps, self.latent_dim*2,), name='latent_sample_in')
+        latent_sample = K.layers.TimeDistributed(
+            K.layers.Lambda(self.reparameterize_layer, output_shape=(self.latent_dim,)),
+            name='latent_sample'
+            )(decoder_in)
+        observations = K.layers.TimeDistributed(
+            K.layers.Dense(units=100, activation='relu'),
+            name='obs1'
+            )(latent_sample)
+        observations = K.layers.TimeDistributed(
+            K.layers.Dense(units=100, activation='relu'),
+            name='obs2'
+            )(observations)
+        observations = K.layers.TimeDistributed(
+            K.layers.Dense(units=100, activation='relu'),
+            name='obs3'
+            )(observations)
+        observations = K.layers.TimeDistributed(
+            K.layers.Dense(units=3*2, activation='softplus'),
+            name='obs_out'
+            )(observations)
 
-        self.observations_model = tf.keras.Sequential(
-            [
-                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
-                tf.layers.Dense(units=100, activation='relu'),
-                tf.layers.Dense(units=100, activation='relu'),
-                tf.layers.Dense(units=100, activation='relu'),
-                tf.layers.Dense(units=3*2, activation='softplus'),
-            ]
-        )
+        rewards = K.layers.TimeDistributed(
+            K.layers.Dense(units=100, activation='relu'),
+            name='rew1'
+            )(latent_sample)
+        rewards = K.layers.TimeDistributed(K.layers.Dense(units=100, activation='relu'),
+            name='rew2'
+            )(rewards)
+        rewards = K.layers.TimeDistributed(
+            K.layers.Dense(units=100, activation='relu'),
+            name='rew3')(rewards)
+        rewards = K.layers.TimeDistributed(
+            K.layers.Dense(units=1*2, activation='sigmoid'),
+            name='rew_out')(rewards)
 
-        self.rewards_model = tf.keras.Sequential(
-            [
-                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
-                tf.layers.Dense(units=100, activation='relu'),
-                tf.layers.Dense(units=100, activation='relu'),
-                tf.layers.Dense(units=100, activation='relu'),
-                tf.layers.Dense(units=1*2, activation='sigmoid'),
-            ]
-        )
+        self.posterior_model = K.Model(inputs=timestep_data, outputs=latent_state, name='encoder')
+        self.decoder_model = K.Model(inputs=decoder_in, outputs=[observations, rewards], name='decoder')
+
+        vae = self.decoder_model(self.posterior_model(timestep_data))
+        self.vae_model = K.Model(inputs=timestep_data, outputs=vae)
 
     def sample(self, eps=None):
         if eps is None:
@@ -70,6 +115,11 @@ class DELIP_model(tf.keras.Model):
 
     def reparameterize(self, mean, logvar):
         eps = tf.random_normal(shape=mean.shape)
+        return eps * tf.exp(logvar * .5) + mean
+
+    def reparameterize_layer(self, input):
+        mean, logvar = tf.split(input, num_or_size_splits=2, axis=1)
+        eps = tf.random_normal(shape=tf.shape(mean))
         return eps * tf.exp(logvar * .5) + mean
 
     # For robot experiment, mebe generate all 3 inference networks at once
@@ -124,95 +174,97 @@ def compute_loss(model, x):
     logqz_x = log_normal_pdf(z, mean, logvar)
     return -tf.reduce_mean(logpx_z + logpz - logqz_x)
 
-
-def compute_loss_combined(model, x, prev_state):
+def compute_loss_combined(model, x):
+    # TODO(slu): state loss
     prior = tfd.Normal(loc=tf.zeros(model.latent_dim), scale=tf.ones(model.latent_dim))
-    mean, log_var = model.infer([x, prev_state])
-    posterior = tfd.Normal(loc=mean, scale=log_var, allow_nan_stats=False)  # TODO(slu): log_var or var
-    latent_sample = model.reparameterize(mean, log_var)
 
-    state_loss = tf.constant([0], dtype=tf.float32)  # TODO(slu): compare to next or current state?
+    latent_state = model.posterior_model(x)
+    latent_mean, latent_logvar = tf.split(latent_state, num_or_size_splits=2, axis=2)
+    latent_sd = tf.exp(latent_logvar*0.5)
+    posterior = tfd.Normal(loc=latent_mean, scale=latent_sd, allow_nan_stats=False)  # TODO(slu): log_var or var NEW: LOG_VAR IS MAKING LOSS HORRIBLE??
 
-    observations_loc, observations_scale = model.generate_observations(latent_sample)
-    observations_d = tfd.Normal(loc=observations_loc, scale=observations_scale, allow_nan_stats=False)
-    observations_loss = observations_d.log_prob(x[0,0,0:3])
+    observations, rewards = model.decoder_model(latent_state)
 
-    rewards_loc, rewards_scale = model.generate_rewards(latent_sample)
-    rewards_d = tfd.Normal(loc=rewards_loc, scale=rewards_scale, allow_nan_stats=False)
-    rewards_loss = rewards_d.log_prob(x[0,0,3:4])
+    observations_mean, observations_logvar = tf.split(observations, num_or_size_splits=2, axis=2)
+    observations_sd = tf.exp(observations_logvar*0.5)
+    observations_d = tfd.Normal(loc=observations_mean, scale=observations_logvar, allow_nan_stats=False)
+    observations_prob = observations_d.log_prob(x[:,:,0:3])
+
+    rewards_mean, rewards_logvar = tf.split(rewards, num_or_size_splits=2, axis=2)
+    rewards_sd = tf.exp(rewards_logvar*0.5)
+    rewards_d = tfd.Normal(loc=rewards_mean, scale=rewards_logvar, allow_nan_stats=False)
+    rewards_prob = rewards_d.log_prob(x[:,:,3:4])
 
     kl_divergence = tfd.kl_divergence(posterior, prior, allow_nan_stats=False)
 
-    return tf.reduce_mean(state_loss) + tf.reduce_mean(observations_loss) + tf.reduce_mean(rewards_loss) - tf.reduce_mean(kl_divergence)  # TODO(slu): reduce_mean or indiviually?
+    return -(tf.reduce_mean(observations_prob) + tf.reduce_mean(rewards_prob) - tf.reduce_mean(kl_divergence))  # TODO(slu): reduce_mean or indiviually?
 
 
-def compute_gradients(model, x, prev_state):
+def compute_gradients(model, x):
     with tf.GradientTape() as tape:
-        loss = compute_loss_combined(model, x, prev_state)
+        loss = compute_loss_combined(model, x)
     return tape.gradient(loss, model.trainable_variables), loss
 
 
 def training():
-    model = DELIP_model(latent_dim=4)
-    optimizer = tf.train.AdamOptimizer(1e-4)
-    train_dataset = generate_dataset(steps=10, episodes=10)
-    test_dataset = generate_dataset(steps=10, episodes=10)
-    epochs = 100
+    trajectory_timesteps = 100
+    total_batch_size = 8000
+    epochs = 10000
+    adam_lr = 1e-4
 
-    prev_state = [[0]*model.latent_dim]
+    model = DELIP_model(latent_dim=4, input_timesteps=trajectory_timesteps)
+
+    optimizer = tf.train.AdamOptimizer(adam_lr)
+    test_dataset = generate_dataset(steps=trajectory_timesteps, episodes=total_batch_size)
+    train_dataset = generate_dataset(steps=trajectory_timesteps, episodes=total_batch_size)
 
     for epoch in range(1, epochs + 1):
         start_time = time.time()
-        for train_x in train_dataset:
-            gradients, loss = compute_gradients(model, train_x, prev_state)
+        for train_x in train_dataset.batch(100):
+            gradients, loss = compute_gradients(model, train_x)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables), global_step=None)
-            prev_state = model.last_latent_state
+            model.reset_states()
         end_time = time.time()
-
-        if epoch % 1 == 0:
-            loss = tfe.metrics.Mean()
-            for test_x in test_dataset:
-                loss(compute_loss(model, test_x))
-            elbo = -loss.result()
-            print('Epoch: {}, Test set ELBO: {}, '
-                  'time elapse for current epoch {}'.format(epoch,
-                                                            elbo,
-                                                            end_time - start_time))
+        print("loss: {}".format(loss))
+        # if epoch % 1 == 0:
+        #     loss = tfe.metrics.Mean()
+        #     for test_x in test_dataset:
+        #         loss(compute_loss(model, test_x))
+        #     elbo = -loss.result()
+        #     print('Epoch: {}, Test set ELBO: {}, '
+        #           'time elapse for current epoch {}'.format(epoch,
+        #                                                     elbo,
+        #                                                     end_time - start_time))
 
     return model
 
 
 def testing():
-    dataset = generate_dataset(10)
-    model = DELIP_model(latent_dim=4)
-    print(model.posterior_model.summary())
-    # print(model.state_generator.summary())
-    # print(model.observations_generator.summary())
-    # print(model.rewards_generator.summary())
+    trajectory_timesteps = 1
+    total_batch_size = 1
 
-    # TODO(slu): Dummy data, remove later
-    dataset_it = dataset.make_one_shot_iterator()
-    temp = dataset_it.get_next()
-    data_tensor = dataset_it.get_next()[0,]
-    data_tensor = tf.reshape(data_tensor, (1, 1, 5))
-    state_tensor = tf.constant([[1]], dtype=tf.float32)
+    model = DELIP_model(latent_dim=4, input_timesteps=trajectory_timesteps)
+    # print(model.vae_model.summary())
+    # K.utils.plot_model(model.vae_model, to_file='vae.png', show_shapes=True)
+    # print(model.posterior_model.summary())
+    # K.utils.plot_model(model.decoder_model, to_file='vae_encoder.png', show_shapes=True)
+    # print(model.decoder_model.summary())
+    # K.utils.plot_model(model.posterior_model, to_file='vae_decoder.png', show_shapes=True)
 
-    gaussians = model.infer([data_tensor, state_tensor])
-    print("Gaussian Params: {}".format(gaussians))
-    samples = model.reparameterize(gaussians[0], gaussians[1])
-    print("Samples: {}".format(samples))
-    # state, observations, rewards = model.generate(samples)
-    # print("State: {}".format(state))
-    # print("Observations: {}".format(observations))
-    # print("Rewards: {}".format(rewards))
-
-    print(compute_loss_combined(model, data_tensor, state_tensor))
-    print("Done")
+    dataset = generate_dataset(steps=trajectory_timesteps, episodes=total_batch_size)
+    for item in dataset.batch(1):
+        res = model.posterior_model(item)
+        print(res)
+        res = model.vae_model(item)
+        print(res)
+        print(compute_loss_combined(model, item))
 
 
 if __name__ == "__main__":
     print("Using Tensorflow Version: {}".format(tf.VERSION))
-    print("Using Keras Version: {}".format(tf.keras.__version__))
-    testing()
+    print("Using Keras Version: {}".format(K.__version__))
+    print(tf.test.gpu_device_name())
+    print(tf.test.is_gpu_available())
+    training()
 
 
